e2257cba8c83adc4b5be51cad7d667ee7bf0b22d
yan <836207753@qq.com>
2022-09-14 15:25:11 +0800

    代码方面配置多语言

--- MJRefresh/NSBundle+MJRefresh.m
+++ MJRefresh/NSBundle+MJRefresh.m
@@ -85,20 +85,15 @@ static NSBundle *mj_systemI18nBundle = nil;
 + (NSBundle *)mj_defaultI18nBundleWithLanguage:(NSString *)language {
     if ([language hasPrefix:@"en"]) {
         language = @"en";
-    } else if ([language hasPrefix:@"zh"]) {
-        if ([language rangeOfString:@"Hans"].location != NSNotFound) {
-            language = @"zh-Hans"; // 简体中文
-        } else { // zh-Hant\zh-HK\zh-TW
-            language = @"zh-Hant"; // 繁體中文
-        }
-    } else if ([language hasPrefix:@"ko"]) {
-        language = @"ko";
-    } else if ([language hasPrefix:@"ru"]) {
-        language = @"ru";
-    } else if ([language hasPrefix:@"uk"]) {
-        language = @"uk";
-    } else {
-        language = @"en";
+    }
+    else if ([language hasPrefix:@"zh"]) {
+        language = @"zh-Hans"; // 简体中文
+    }
+    else if ([language hasPrefix:@"ja"]) {
+        language = @"ja";
+    }
+    else {
+        language = @"ja";
     }
     
     // 从MJRefresh.bundle中查找资源
